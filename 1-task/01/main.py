7a59a17cd885b83c0a37d514969234d628e8db17
1 <seredin781@gmail.com>
2023-11-01 18:16:44 +0900

    iwritethiscode

--- 1-task/01/main.py
+++ 1-task/01/main.py
@@ -1,2 +1,8 @@
 # Здесь пиши программу
-
+spisok = []
+long_spiska = int(input("введите длину списка "))
+for i_long in range(long_spiska):
+    if (i_long % 2 != 0):
+        spisok.append(i_long)
+    i_long = i_long + 1
+print(spisok)
