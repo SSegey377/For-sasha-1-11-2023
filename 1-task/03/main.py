eac82aa34dc73d0b7891d2052d49b1e2af0c0cf1
1 <seredin781@gmail.com>
2023-11-06 21:21:46 +0900

    iwritethiscode

--- 1-task/03/main.py
+++ 1-task/03/main.py
@@ -1,2 +1,13 @@
 # Здесь пиши программу
+films = ['Крепкий орешек', 'Назад в будущее', 'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 'Мементо', 'Отступники', 'Деревня']
+myplaylist = []
+a = int(input("сколько фильмов хотите добавить: "))
 
+for i in range(a):
+    movie_name = input("введите название фильма ").title()
+    rule = films.index(movie_name)
+    if rule != 0:
+        myplaylist.append((movie_name))
+    else:
+        print("Фильма",movie_name, "нет :(")
+print(myplaylist)
