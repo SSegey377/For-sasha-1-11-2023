a7a93e260d981bf4c3db7d1bce423bab115272b1
1 <seredin781@gmail.com>
2023-11-13 22:01:37 +0900

    iwritethiscode

--- 1-task/08/main.py
+++ 1-task/08/main.py
@@ -1,2 +1,7 @@
 # Здесь пиши программу
-
+spicok_chisel = []
+kolichestvo_chisel = int(input('Введите кол-во чисел для сортировки'))
+for i in range(kolichestvo_chisel):
+    chislo = int(input('Введите число> '))
+    spicok_chisel.append(chislo)
+print (sorted(spicok_chisel))
