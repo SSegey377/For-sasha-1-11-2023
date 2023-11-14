67059fd684f6a661df056f24672177a4384e2c79
1 <seredin781@gmail.com>
2023-11-14 17:08:12 +0900

    iwritethiscode

--- 1-task/06/main.py
+++ 1-task/06/main.py
@@ -1,5 +1,6 @@
 # Здесь пиши программу
 spicok = [1,2,3,4,5,6,7,8,9]
 sdvig = int(input("Введите сдвиг списка: "))
-spicok = spicok[-sdvig:] + spicok[:-sdvig]
+a = sdvig%len(spicok)
+spicok = spicok[-a:] + spicok[:-a]
 print(spicok)
