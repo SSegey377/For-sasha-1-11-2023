283865894e8e32027ad6d0652a76809567f7839c
1 <seredin781@gmail.com>
2023-11-16 20:11:54 +0900

    iwritethiscode

--- 1-task/08/main.py
+++ 1-task/08/main.py
@@ -1,7 +1,22 @@
 # Здесь пиши программу
-spicok_chisel = []
-kolichestvo_chisel = int(input('Введите кол-во чисел для сортировки'))
-for i in range(kolichestvo_chisel):
-    chislo = int(input('Введите число> '))
-    spicok_chisel.append(chislo)
-print (sorted(spicok_chisel)) # условия нелься использовать
+spicok_chisel =[1, 4, -3, 0, 10]
+# kolichestvo_chisel = int(input('Введите кол-во чисел для сортировки: '))
+# dop_list = []
+# for i in range(kolichestvo_chisel):
+#     chislo = int(input('Введите число: '))
+#     spicok_chisel.append(chislo)
+# while spicok_chisel:
+#     minimum = spicok_chisel[0]
+#     for x in spicok_chisel:
+#         if x < minimum:
+#             minimum = x
+#     dop_list.append(minimum)
+#     spicok_chisel.remove(minimum)
+#
+# print(dop_list)
+for i in range(len(spicok_chisel)):
+    for j in range(i+1,len(spicok_chisel)):
+        if spicok_chisel[i] >= spicok_chisel[j]:
+            spicok_chisel[i],spicok_chisel[j]=spicok_chisel[j],spicok_chisel[i]
+
+print(spicok_chisel)
--- 1-task/09/main.py
+++ 1-task/09/main.py
@@ -1,2 +1,9 @@
 # Здесь пиши программу
-
+spicok_long = int(input("введите длину списка "))
+spicok = []
+for i in range(spicok_long):
+    chislo = int(input("Введите число: "))
+    if chislo % 2== 0:
+        spicok.append(chislo)
+for b in range (len(spicok)-1,-1,-1):
+    print(spicok[b])
