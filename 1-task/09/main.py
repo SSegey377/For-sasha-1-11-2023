ada9a9026f73cc459180aeacc4685ad4b88da955
1 <seredin781@gmail.com>
2023-11-17 21:44:44 +0900

    iwritethiscode

--- 1-task/09/main.py
+++ 1-task/09/main.py
@@ -5,10 +5,11 @@ for i in range(spicok_long):
     chislo = int(input("Введите число: "))
     if chislo % 2== 0:
         spicok.append(chislo)
-for b in range (len(spicok)-1,-1,-1):
-    print(spicok[b])
-
-
+for i in range(len(spicok)):
+    for j in range(i+1,len(spicok)):
+        if spicok[i] <= spicok[j]:
+            spicok[j],spicok[i]=spicok[i],spicok[j]
+print(spicok)
 # не верно. нужно список который дан. сравнивать первое число со вторым, потом второе с третим и т.д.
 
 # посмотри задачу 8. 
