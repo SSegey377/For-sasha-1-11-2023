a9d697bd6fec9b2af002b7bdb3caf7315e3c698f
1 <seredin781@gmail.com>
2023-11-30 19:26:47 +0900

    iwritethiscode

--- 2-task-/01/main.py
+++ 2-task-/01/main.py
@@ -1,2 +1,11 @@
 # Здесь пиши программу
-
+base_spicok = [1, 5, 3,5,5,5,5,5,5,5,5]
+spicok2 = [1, 5, 1, 5]
+spicok3 = [1, 3, 1, 5, 3, 3]
+base_spicok = base_spicok + spicok2
+print("Кол-во цифр 5 при первом объединении:",base_spicok.count(5))
+for i in range(base_spicok.count(5)):
+        base_spicok.remove(5)
+base_spicok = base_spicok + spicok3
+print("Кол-во цифр 3 при втором объединении:", base_spicok.count(3))
+print("Конечный список", base_spicok)
