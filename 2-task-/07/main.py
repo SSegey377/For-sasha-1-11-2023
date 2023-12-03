d1e3a6e0187ce0cb6ee2e38394ebf4e7b8654db8
1 <seredin781@gmail.com>
2023-12-03 17:42:26 +0900

    iwritethiscode

--- 2-task-/07/main.py
+++ 2-task-/07/main.py
@@ -1,2 +1,20 @@
 # Здесь пиши программу
-
+chel_v_kruge = []
+chel = int(input("введите число людей "))
+for i in range(chel):
+    chel_v_kruge.append(i+1)
+schitalka = int(input("введите на какой счет выбывает человек "))
+while 1 > 0:
+    if len(chel_v_kruge) > 1:
+        if schitalka > len(chel_v_kruge):
+            schitalka = schitalka - len(chel_v_kruge)
+            print("выбыл человек: ", chel_v_kruge[schitalka - 1])
+            chel_v_kruge.remove(chel_v_kruge[schitalka-1])
+            print("оставшиеся люди ",chel_v_kruge)
+        else:
+            print("выбыл человек: ",chel_v_kruge[schitalka-1])
+            chel_v_kruge.remove(chel_v_kruge[schitalka-1])
+            print("оставшиеся люди ",chel_v_kruge)
+    else:
+        break
+print("остался человек под номером ",chel_v_kruge[0])
--- 2-task-/08/main.py
+++ 2-task-/08/main.py
@@ -1,2 +1,33 @@
 # Здесь пиши программу
+def palidrom(spicok_test):
+    spicok_f = []
+    for v in range(len(spicok_test) -1, -1, -1):
+        spicok_f.append(spicok_test[v])
+    if spicok_test == spicok_f:
+        return True
+    else:
+        return False
 
+spicok = []
+spicok_2 = []
+void_spicok = []
+chislo = int(input('введите кол-во чисел '))
+for i in range(chislo):
+    chislo_v_spicke = int(input("Число "))
+    spicok.append(chislo_v_spicke)
+
+
+for u in range(len(spicok)):
+    for k in range(u,len(spicok)):
+        void_spicok.append(spicok[k])
+
+    if palidrom(void_spicok):
+        for l in range(u):
+            spicok_2.append(spicok[l])
+
+        spicok_2.reverse()
+        break
+    void_spicok = []
+print('Последовательность: ', spicok)
+print('Нужно приписать чисел: ', len(spicok_2))
+print('Сами числа:', spicok_2)
