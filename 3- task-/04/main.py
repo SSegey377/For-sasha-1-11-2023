b57479fe372da9d69bbe47f300f4a8c32e994479
1 <seredin781@gmail.com>
2023-12-04 20:27:45 +0900

    iwritethiscode

--- 3- task-/04/main.py
+++ 3- task-/04/main.py
@@ -1,2 +1,13 @@
 # Здесь пиши программу
-
+alph_eng = 'abcdefg'
+rev_str = alph_eng[::-1]
+print(alph_eng)
+print(rev_str)
+print(alph_eng[::2])
+print(alph_eng[1::2])
+print(alph_eng[0])
+print(alph_eng[-1])
+print(alph_eng[3])
+print(alph_eng[3:4:-1])
+print(alph_eng[3:4])
+print(alph_eng[-3:-4])
