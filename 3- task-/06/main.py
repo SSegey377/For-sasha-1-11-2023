5de0e3d57de9c9e75dd592fcfffa7a320906e45b
1 <seredin781@gmail.com>
2023-12-11 18:11:03 +0900

    iwritethiscode

--- 3- task-/06/main.py
+++ 3- task-/06/main.py
@@ -1,2 +1,6 @@
 # Здесь пиши программу
+sp_long = 12
+sp_dop_int = 4
+sp = [[i_num + i for i_num in range(0, sp_long, 4)]for i in range(1, sp_dop_int + 1)]
 
+print(sp)
--- 3- task-/08/main.py
+++ 3- task-/08/main.py
@@ -1,2 +1,17 @@
 # Здесь пиши программу
+def split(s):
+    return [char for char in s]
 
+alp = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"]
+shifr = str(input("введите текст: "))
+sdvg = int(input("введите сдвиг: "))
+sp = split(shifr)
+string = ""
+print(sp)
+for i in range(len(sp)):
+    if sp[i] in alp:
+        c = []
+        c.append(alp[alp.index(sp[i])+sdvg])
+    for el in c:
+        string += el
+print(string)
