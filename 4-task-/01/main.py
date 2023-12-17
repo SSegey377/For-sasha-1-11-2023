6e8991c3ed3ce66e18cb912210d9f9a00652587c
1 <seredin781@gmail.com>
2023-12-17 19:12:04 +0900

    iwritethiscode

--- 4-task-/01/main.py
+++ 4-task-/01/main.py
@@ -1,2 +1,11 @@
 # Здесь пиши программу
+unlock_menu = []
+skoka_blud = int(input("введите количество блюд в меню: "))
+dop_str = str
+for i in range(skoka_blud):
+    bludo = input('введите название блюда: ').split(";")
+    unlock_menu.append(bludo)
+    dop_str = dop_str + unlock_menu[i]
+print('Доступное меню:',dop_str)
+print(unlock_menu[0])
 
