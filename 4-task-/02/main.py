8606efcd67f93f73d85bc6a9d15bd19d65d9625f
1 <seredin781@gmail.com>
2023-12-19 20:12:57 +0900

    iwritethiscode

--- 4-task-/02/main.py
+++ 4-task-/02/main.py
@@ -1,2 +1,4 @@
 # Здесь пиши программу
-
+stroka = str(input('введите строку: ')).split()
+print('Наибольшее слово: ',max(stroka, key=len))
+print('Длина наибольшего слова: ',len(max(stroka, key=len)))
