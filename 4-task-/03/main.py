2c2992557cafbf6f85212b5c5f6ef8b2828725a5
1 <seredin781@gmail.com>
2023-12-23 08:01:25 +0900

    iwritethiscode

--- 4-task-/03/main.py
+++ 4-task-/03/main.py
@@ -2,8 +2,7 @@
 dangerous_symbols = ['|', '/', ''' \ ''' , "№", "$", "%", "^", "&",  '*', '(','@', ')']
 domain_files = ["txt","docx"]
 dokument = str(input('Введите файл: '))
-# print(len(dokument))
-# for i in range(len(dokument)):
+
 if dokument not in dangerous_symbols:
     if dokument not in domain_files:
         print('файл неверного формата или имеет специальные символы')
--- 4-task-/04/main.py
+++ 4-task-/04/main.py
@@ -1,2 +1,3 @@
 # Здесь пиши программу
-
+stroka = str(input("введите строку: "))
+print(stroka.title())
