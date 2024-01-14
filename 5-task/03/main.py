5236e272681d419e58d62a1a02c7e9a7807ac1b9
1 <seredin781@gmail.com>
2024-01-14 12:01:35 +0900

    iwritethiscode

--- 5-task/03/main.py
+++ 5-task/03/main.py
@@ -1,2 +1,37 @@
 # Здесь пиши программу
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
 
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+for key, val in goods.items():
+    a = 0
+    b = 0
+    if len(store[val])<= 1:
+        a = store[val][0]['quantity'] * store[val][0]['price']
+        b = store[val][0]['quantity']
+    else:
+        for i in range(len(store[val])):
+            a = store[val][i]['quantity'] * store[val][i]['price'] + a
+            b = store[val][i]['quantity'] + b
+    print(key," - ",b,' штук, стоимость ',a,' рублей')
