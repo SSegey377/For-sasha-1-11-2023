ad4c493e47ba9ebb5af4051b016a2fddf1ecd8f2
1 <seredin781@gmail.com>
2024-02-16 19:59:08 +0900

    iwritethiscode

--- 5-task/07/main.py
+++ 5-task/07/main.py
@@ -1,2 +1,18 @@
 # Здесь пиши программу
+array_1 = [1, 5, 10, 20, 40, 80, 100]
+array_2 = [6, 7, 20, 80, 100]
+array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
 
+res = [num for num in [elem for elem in array_1 if elem in array_2] if num in array_3]
+d = sorted(set(array_1) & set(array_2) & set(array_3))
+
+res_2 = [num for num in [elem for elem in array_1 if elem not in array_2] if num not in array_3]
+d_2 = sorted(set(array_1) - set(array_2) - set(array_3))
+
+print('Задача 1:')
+print('\n * Решение без множеств:', *res)
+print(' * Решение с множествами:', *d)
+
+print('Задача 2:')
+print('\n * Решение без множеств:', *res_2)
+print(' * Решение с множествами:', *d_2)
--- 5-task/08/main.py
+++ 5-task/08/main.py
@@ -1,2 +1,19 @@
 # Здесь пиши программу
+def equal(x):
+    d = x.copy()
+    d.remove(d[-1])
+    for s in range(len(d)):
+        if d.count(d[s])%2<=0:
+            while d.count(d[s])%2>=0:
+                s+=1
+                break
+            print('Можно сделать палиндромом')
+        else:
+           print('Нельзя сделать палиндромом')
+        break
 
+c = input('введите строку: ')
+b = []
+for i in range(len(c)):
+    b.append(c[i])
+equal(b)
